49da6a7c253da71cdba5f144b980cc488edc2578
Takeru Hayashi <takeruhayashi@g.ucla.edu>
2025-05-21 16:53:45 -0700

    UPDATE: read_data() in ad7124.py

--- src/hs_temp_sensor/ad7124.py
+++ src/hs_temp_sensor/ad7124.py
@@ -1,4 +1,5 @@
 import spidev
+import time
 
 AD7124_SPI_BUS = 0
 AD7124_SPI_DEVICE = 0
@@ -94,6 +95,7 @@ class AD7124:
         print("Channel {} Configuration: 0x{:04X}".format(channel, channel_config_reg))
         
     def read_data(self):
+        time.sleep(0.001)
         comms_write = AD7124_COMMS_REG | AD7124_COMM_REG_WEN | AD7124_COMM_REG_RD | AD7124_COMM_REG_RA(AD7124_DATA_REG)
         data_reg = self.spi.xfer2([comms_write, 0x00, 0x00, 0x00])
         data = (data_reg[-3] << 16) | (data_reg[-2] << 8) | data_reg[-1]
