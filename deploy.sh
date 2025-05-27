72d3f430f6fa03fa70ae30da9c85af2821b70fad
Takeru Hayashi <takeruhayashi@g.ucla.edu>
2025-05-27 16:31:48 -0700

    UPDATE: getopts to GNU's getopt

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,44 @@
 #! /usr/bin/bash
 
-while getopts ":vrith" OPTARG; do
-    case $OPTARG in
-    v)
+short_opt_str=':vrih'
+long_opt_str=':verbose,reset,id,temp,test,help'
+
+VALID_ARGS=$(getopt -o "$short_opt_str" -l "$long_opt_str" -- "$@")
+if [ $? -ne 0 ]; then
+    echo "Invalid arguments. Use -h for help."
+    exit 1;
+fi
+
+eval set -- "$VALID_ARGS"
+while true; do
+    case "$1" in
+    '-v'|'--verbose')
         COMMAND="uv run src/hs_temp_sensor -vvvv"
+        break
         ;;  
-    r)
+    '-r'|'--reset')
         COMMAND="uv run src/hs_temp_sensor -r -vvvv"
+        break
         ;;
-    i)
+    '-i'|'--id')
         COMMAND="uv run src/hs_temp_sensor --id -vvvv"
+        break
         ;;
-    t)
+    '--temp')
         COMMAND="uv run src/hs_temp_sensor --temp -vvvv"
+        break
         ;;
-    h)
+    '--test')
+        COMMAND="uv run src/hs_temp_sensor --test -vvvv"
+        break
+        ;;
+    '--')
+        shift
+        break
+        ;;
+    '-h'|'--help')
         echo "Help: deploy.sh [-v] [-r] [-h]"
         exit 0
-        ;;
-    \?)
-        echo "Invalid option: -$OPTARG" >&2
-        exit 1
         ;;
     esac
 done
@@ -33,4 +51,4 @@ PRECOMMANDS+="uv sync > /dev/null 2>&1 &&"
 
 ssh $SERVER $PRECOMMANDS $COMMAND;
 
-exit;
+exit 0;
