025fdc3818f56fb42a1bbf9b226f8e757a9e1311
Takeru Hayashi <takeruhayashi@g.ucla.edu>
2025-05-28 12:08:02 -0700

    UPDATE: __main__.py

--- src/hs_temp_sensor/__main__.py
+++ src/hs_temp_sensor/__main__.py
@@ -64,24 +64,25 @@ def main() -> None:
         # for i in range(16):
         #     adc.read_channel_config(i)
         adc.read_adc_config()
+        adc.read_config(cfg_channel=0)
         adc.read_channel_config(0)
         adc.read_channel_config(1)
         adc.read_io_control(io_channel=1)
-        adc.read_config(cfg_channel=0)
+        
         
         adc.set_adc_config()
+        adc.set_config(cfg_channel=0)
         adc.set_channel_config(channel=0, setup=0, ainp=16, ainm=17)
         adc.set_channel_config(channel=1, setup=0, ainp=1, ainm=2)
         adc.set_io_control(io_channel=1)
-        adc.set_config(cfg_channel=0)
         # for i in range(16):
         #     adc.read_channel_config(i)
         
         adc.read_adc_config()
+        adc.read_config(cfg_channel=0)
         adc.read_channel_config(0)
         adc.read_channel_config(1)
         adc.read_io_control(io_channel=1)
-        adc.read_config(cfg_channel=0)
 
         # for i in range(16):
         #     data = adc.read_data()
