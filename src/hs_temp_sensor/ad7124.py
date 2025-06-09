fd1c95c26395a9672c191b86128bab4d626c35df
Takeru Hayashi <takeruhayashi@g.ucla.edu>
2025-06-09 14:52:10 -0700

    UPDATE: ad7124.py

--- src/hs_temp_sensor/ad7124.py
+++ src/hs_temp_sensor/ad7124.py
@@ -103,7 +103,7 @@ class AD7124:
         
     def reset(self):
         self.spi.xfer2([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
-        logger.debug("Reset complete")
+        # logger.debug("Reset complete")
         
     def read_status(self):
         comms_write = AD7124_COMMS_REG | AD7124_COMM_REG_WEN | AD7124_COMM_REG_RD | AD7124_COMM_REG_RA(AD7124_STATUS_REG)
