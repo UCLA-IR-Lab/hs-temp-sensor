58dc481a2bdd12ea41bc1117d1e11b66ef3ce357
Takeru Hayashi <takeruhayashi@g.ucla.edu>
2025-06-09 14:02:25 -0700

    UPDATE: __main__.py

--- src/hs_temp_sensor/__main__.py
+++ src/hs_temp_sensor/__main__.py
@@ -61,6 +61,7 @@ def main() -> None:
         adc.set_adc_config()
         adc.set_config(gain=1, cfg_channel=0)
         adc.set_channel_config(channel=0, setup=0, ainp=16, ainm=17)
+        adc.read_status()
         # if args.verbosity:
         #     adc.read_adc_config()
         #     adc.read_channel_config()
